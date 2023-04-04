591a2bbbc91d12afbb707efce06243fa6d4ea1f2
DiepHoaiAn <diephoaian2003@gmail.com>
2023-04-04 19:26:43 +0700

    update maze_displayer.py

--- maze_displayer.py
+++ maze_displayer.py
@@ -1,26 +1,30 @@
 import pygame
 from helper import drawMaze, getJsonData
-from constants import WINDOW, CELL_SIZE, MAP_IMAGE, COIN_IMAGE, OBSTACLE_IMAGE, BOT_IMAGE_LIST
+from constants import CELL_SIZE, BOARD_SIZE, SCALE_RATIO, MAP_IMAGE, COIN_IMAGE, OBSTACLE_IMAGE, BOT_IMAGE_LIST
 
-MAZE = getJsonData('maze_metadata.json')
-CONFIG = {
-    'size': CELL_SIZE,
-    'background': MAP_IMAGE,
-    'coinImage': COIN_IMAGE,
-    'obstacleImage': OBSTACLE_IMAGE,
-    'botImageList': BOT_IMAGE_LIST
-}
+class MazeDisplayer:
+    def __init__(self, maze_metadata):
+        self.maze = getJsonData(maze_metadata)
+        self.config = {
+            'size': CELL_SIZE,
+            'background': MAP_IMAGE,
+            'coinImage': COIN_IMAGE,
+            'obstacleImage': OBSTACLE_IMAGE,
+            'botImageList': BOT_IMAGE_LIST
+        }
+        self.window = pygame.display.set_mode(BOARD_SIZE)
 
+    def render(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-def render():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            drawMaze(self.window, self.maze, self.config)
 
-        drawMaze(WINDOW, MAZE, CONFIG)
-
-
+			
 if __name__ == "__main__":
-    render()
+    md = MazeDisplayer('maze_metadata.json')
+    md.render()
+
